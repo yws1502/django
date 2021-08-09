8f28fd49e714f97650594c533d217e50c1c1a84b
Woosang <yws1502@naver.com>
2021-08-09 18:54:15 +0900

    clean up

--- 05_Dstargram/config/settings.py
+++ 05_Dstargram/config/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# secret_file = os.
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
