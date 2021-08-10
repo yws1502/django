61621a1fefe0b964967ff3f2402a4535c834c270
Woosang <yws1502@naver.com>
2021-08-11 00:16:15 +0900

    edit files

--- 05_Dstargram/photo/urls.py
+++ 05_Dstargram/photo/urls.py
@@ -5,5 +5,7 @@ from .views import *
 app_name = 'photo' # name space
 urlpatterns = [
     path('', photo_list, name='photo_list'),
-
+    path('upload/', PhotoUploadView.as_view(), name='photo_upload'),
+    path('delete/<int:pk>/', PhotoDeleteView.as_view(), name='photo_delete'),
+    
 ]
--- 05_Dstargram/photo/views.py
+++ 05_Dstargram/photo/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.shortcuts import redirect
 from .models import Photo
 
 def photo_list(request):
     # 보여줄 사진 데이터
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos':photos})# object_list가 관례적이다 바꿀 수 있다.
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    # 작성자 필수 필드
+    fields = ['photo', 'text'] # 작성자(author), 작성시간(created) -> auto_now_add 덕분에 알아서 들어감
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            # 데이터가 올바르다면 처리
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
