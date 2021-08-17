3f0254c61dc9e7a0c5948875831644ec85461cd8
Woosang <yws1502@naver.com>
2021-08-18 01:38:58 +0900

    create mixin, Decorator

--- 05_Dstargram/photo/views.py
+++ 05_Dstargram/photo/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 from .models import Photo
 
+@login_required
 def photo_list(request):
     # 보여줄 사진 데이터
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos':photos})# object_list가 관례적이다 바꿀 수 있다.
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     # 작성자 필수 필드
     fields = ['photo', 'text'] # 작성자(author), 작성시간(created) -> auto_now_add 덕분에 알아서 들어감
@@ -23,12 +25,12 @@ class PhotoUploadView(CreateView):
         else:
             return self.render_to_response({'form':form})
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
